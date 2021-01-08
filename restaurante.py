2ce1b31c3f97b11231c785e77efe10a51afbb1e1
luis Debia V <luis.debia@gmail.com>
2021-01-08 08:50:08 -0300

    Otro mas finalizado

--- restaurante.py
+++ restaurante.py
@@ -1,11 +1,17 @@
+import os
+os.system('cls')
+
 
 # PARTE 1) Restaurante:
+numero_servicio=0
+n=0
+s=""
+class Restaurante:
 
-
-class Restaurante :
-    def __init__(self, restaurant_nombre,cocina_tipo):
+    def __init__(self, restaurant_nombre,cocina_tipo,numero_servicio=0):
         self.restaurant_nombre = restaurant_nombre
         self.cocina_tipo = cocina_tipo
+        
 
 def describe_restaurant():
         print (restaurante.restaurant_nombre,restaurante.cocina_tipo)
@@ -13,7 +19,7 @@ def describe_restaurant():
 
 def abrir_restaurant():
         print('El Restaurante esta abierto')
-    
+
 restaurante = Restaurante ("Las Delicias", "Mariscos")
 
 print(restaurante.restaurant_nombre)
@@ -29,7 +35,32 @@ print()
 restaurante1 = Restaurante ("Las Delicias_1", "Pastas")
 restaurante2 = Restaurante ("Las Delicias_2", "Vegetales")
 restaurante3 = Restaurante ("Las Delicias_3", "Pasteles")
-
+print()
 print(restaurante1.restaurant_nombre,restaurante.cocina_tipo)
 print(restaurante2.restaurant_nombre,restaurante.cocina_tipo)
 print(restaurante3.restaurant_nombre,restaurante.cocina_tipo)
+print()
+# PARTE 3) Número servicio: 
+print()
+while s!='s':
+    print()
+    s=str(input('Otro Nombre ( "S"=Finalizar "Enter" = Continuar  :)'))
+    s=s.lower()
+    if s!="s":
+        n= n + 1
+        print()
+        print("Atendiendo al cliente   :",n)
+        numero_servicio=numero_servicio + 1
+
+def set_numero_servicio():
+    print("Atendiendo al cliente    :",numero_servicio)
+
+def incrementar_numero_servicio():
+    print()
+    os.system('cls')
+    print(restaurante.restaurant_nombre)
+    print("Número de clientes atendidos en el día  :",numero_servicio)
+
+
+set_numero_servicio()
+incrementar_numero_servicio()
